29dd7207c9182972e2d5c2a7f59a71459e7aee39
dengliangliang <a1519394854@163.com>
2025-11-21 02:39:00 +0800

    fix: 添加，修复CORS配置允许所有域名

--- 1213yixuesuanming/1213yixuesuanming/src/main.js
+++ 1213yixuesuanming/1213yixuesuanming/src/main.js
@@ -4,6 +4,7 @@ import uviewPlus from 'uview-plus'
 import util from '@/common/util.js'
 import API from "@/common/request/api.js"
 import website from './config/website'
+import '@/common/hooks/index.js' // 导入全局钩子，包含$toast等方法
 
 export function createApp() {
 	const app = createSSRApp(App)
@@ -12,6 +13,11 @@ export function createApp() {
 	app.config.globalProperties.util = util;
 	app.config.globalProperties.$api = API;
 	app.config.globalProperties.$config = website;
+	// 添加$toast方法到全局属性
+	app.config.globalProperties.$toast = (title) => uni.showToast({
+		title,
+		icon: "none"
+	});
 	
 	// 使用 uView Plus
 	app.use(uviewPlus)
